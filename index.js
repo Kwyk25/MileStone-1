8a18a4f8cc4eefa921e82048fa3bc61b84c653a8
Kyle <kylev006@gmail.com>
2023-03-25 11:29:33 -0700

    found bug that make pipe be out of bounds

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var pipes = document.getElementById("block");
 var gap = document.getElementById("gap");
 
 gap.addEventListener('animationiteration', () => {
-    var random = -((Math.random()*750)+200);
+    var random = -((Math.random()*500)+200);
     gap.style.top = random + "px";
     counter++;
 });
