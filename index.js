bd524bfd89fedaa8d0906b920ae9578bfc384d13
Kyle <kylev006@gmail.com>
2023-03-25 11:58:34 -0700

    added gravity

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 var pipes = document.getElementById("block");
 var gap = document.getElementById("gap");
+var chara = document.getElementById("chara");
 
 gap.addEventListener('animationiteration', () => {
     var random = -((Math.random()*500)+200);
     gap.style.top = random + "px";
     counter++;
 });
+
+setInterval(function(){
+    var charaTop = parseInt(window.getComputedStyle(chara).getPropertyValue("top"));
+    chara.style.top = (charaTop+2)+"px"
+},10)
