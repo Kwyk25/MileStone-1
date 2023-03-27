a47ea4af931dbd818813124d3b41ce61f30a260a
Kyle <kylev006@gmail.com>
2023-03-27 13:20:39 -0700

    got jumping

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var pipes = document.getElementById("block");
 var gap = document.getElementById("gap");
 var chara = document.getElementById("chara");
+var jumping = 0;
 
 gap.addEventListener('animationiteration', () => {
     var random = -((Math.random()*500)+200);
@@ -10,5 +11,24 @@ gap.addEventListener('animationiteration', () => {
 
 setInterval(function(){
     var charaTop = parseInt(window.getComputedStyle(chara).getPropertyValue("top"));
-    chara.style.top = (charaTop+2)+"px"
+    if(jumping==0){
+        chara.style.top = (charaTop+3)+"px"
+    }
 },10)
+
+function jump(){
+    jumping = 1;
+    let jumpCount = 0
+    var jumpInt = setInterval(function(){
+        var charaTop = parseInt(window.getComputedStyle(chara).getPropertyValue("top"));
+        
+            chara.style.top = (charaTop-5)+"px"
+        
+            if(jumpCount>20){
+                clearInterval(jumpInt);
+                jumping = 0;
+                jumpCount = 0;
+            }
+        jumpCount++;
+    },10)
+}
