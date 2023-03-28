4242267a15b693e679a948281f508a70e5260407
Kyle <kylev006@gmail.com>
2023-03-27 18:39:09 -0700

    added ball move thru but with a bug

--- index.js
+++ index.js
@@ -1,34 +1,8 @@
-var pipes = document.getElementById("block");
+var pipes = document.getElementById("pipes");
 var gap = document.getElementById("gap");
 var chara = document.getElementById("chara");
 var jumping = 0;
-
-let playerPos = chara.getBoundingClientRect();
-let grassPos = grass.getBoundingClientRect();
-
-const JUMPSPEED = 60
-const GRAVITYMAIN = 2
-
-let gravity = GRAVITYMAIN
-let gravityDec = 0.6
-let terminalVec = 5
-let impulse = 0
-
-let flappyMoveIntId;
-
-function start(){
-    clearInterval(flappyMoveIntId)
-    flappyMoveIntId = setInterval(moveFlappy,10) 
-}
-
-function moveFlappy(){
-    var charaTop = parseInt(window.getComputedStyle(chara).getPropertyValue("top"));
-    let newTop = charaTop + (gravity - impulse)
-    impulse = Math.max(0, impulse*gravityDec)
-    gravity = Math.min(terminalVec, gravity/gravityDec)
-    chara.style.top = `${newTop}px`;
-}
-
+var counter = 0;
 
 gap.addEventListener('animationiteration', () => {
     var random = -((Math.random()*500)+200);
@@ -39,23 +13,31 @@ gap.addEventListener('animationiteration', () => {
 setInterval(function(){
     var charaTop = parseInt(window.getComputedStyle(chara).getPropertyValue("top"));
     if(jumping==0){
-        chara.style.top = (charaTop+3)+"px"
+        chara.style.top = (charaTop+2)+"px";
     }
-},10)
+    var pipesLeft = parseInt(window.getComputedStyle(pipes).getPropertyValue("left"));
+    var gapTop = parseInt(window.getComputedStyle(gap).getPropertyValue("top"));
+    var cTop = -(500-charaTop);
+    if((charaTop>659)||((pipesLeft<188)&&(pipesLeft>-50)&&((cTop<gapTop)||(cTop>gapTop+150)))){
+        alert("Game over. Score: "+(counter -1));
+        chara.style.top = 200 + "px";
+        counter=0;
+    }
+},10);
 
 function jump(){
     jumping = 1;
-    let jumpCount = 0
+    let jumpCount = 0;
     var jumpInt = setInterval(function(){
         var charaTop = parseInt(window.getComputedStyle(chara).getPropertyValue("top"));
-        
-            chara.style.top = (charaTop-5)+"px"
-        
-            if(jumpCount>20){
-                clearInterval(jumpInt);
-                jumping = 0;
-                jumpCount = 0;
-            }
+        if((charaTop>6)&&(jumpCount<15)){
+            chara.style.top = (charaTop-4)+"px";
+        }
+        if(jumpCount>20){
+            clearInterval(jumpInt);
+            jumping=0;
+            jumpCount=0;
+        }
         jumpCount++;
-    },10)
+    },10);
 }
