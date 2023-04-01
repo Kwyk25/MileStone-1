d21ad829dcbb4d029eff68d222948e0660beca34
Kyle <kylev006@gmail.com>
2023-04-01 12:13:29 -0700

    fixed bugs and increased pipe speed

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ setInterval(function(){
     var pipesLeft = parseInt(window.getComputedStyle(pipes).getPropertyValue("left"));
     var gapTop = parseInt(window.getComputedStyle(gap).getPropertyValue("top"));
     var cTop = -(500-charaTop);
-    if((charaTop>659)||((pipesLeft<188)&&(pipesLeft>-70)&&((cTop<gapTop+170)||(cTop>gapTop+360)))){
+    if((charaTop>659)||((pipesLeft<191)&&(pipesLeft>-70)&&((cTop<gapTop+253)||(cTop>gapTop+359)))){
         alert("Game over. Score: "+(counter -1));
         chara.style.top = 200 + "px";
         counter=0;
@@ -31,7 +31,7 @@ function jump(){
     var jumpInt = setInterval(function(){
         var charaTop = parseInt(window.getComputedStyle(chara).getPropertyValue("top"));
         if((charaTop>6)&&(jumpCount<15)){
-            chara.style.top = (charaTop-4)+"px";
+            chara.style.top = (charaTop-6)+"px";
         }
         if(jumpCount>20){
             clearInterval(jumpInt);
