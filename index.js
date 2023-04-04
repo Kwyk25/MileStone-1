81c0eb14f3f9dcaea0bb65b55cfee760e1e56183
Kyle <kylev006@gmail.com>
2023-04-03 20:14:52 -0700

    fixed bugs and added sounds

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ gap.addEventListener('animationiteration', () => {
 });
 
 setInterval(function(){
+    // var audio = new Audio("./audio/deathSound.mp3");
     var charaTop = parseInt(window.getComputedStyle(chara).getPropertyValue("top"));
     if(jumping==0){
         chara.style.top = (charaTop+2)+"px";
@@ -18,20 +19,22 @@ setInterval(function(){
     var pipesLeft = parseInt(window.getComputedStyle(pipes).getPropertyValue("left"));
     var gapTop = parseInt(window.getComputedStyle(gap).getPropertyValue("top"));
     var cTop = -(500-charaTop);
-    if((charaTop>659)||((pipesLeft<191)&&(pipesLeft>-70)&&((cTop<gapTop+253)||(cTop>gapTop+359)))){
-        alert("Game over. Score: "+(counter -1));
+    if(/*ground*/(charaTop>688)||(/*right to left*/(pipesLeft<186)&&/*top pipe*/(pipesLeft>-10)&&(/*top pipe*/(cTop<gapTop+247)||/*bottom pipe*/(cTop>gapTop+388)))){
+        alert("Game over. Score: "+(counter -1)); 
         chara.style.top = 200 + "px";
         counter=0;
-    }
+    }parse
 },10);
 
 function jump(){
+    var audio = new Audio("./audio/sfx_wing.mp3");
     jumping = 1;
     let jumpCount = 0;
     var jumpInt = setInterval(function(){
         var charaTop = parseInt(window.getComputedStyle(chara).getPropertyValue("top"));
         if((charaTop>6)&&(jumpCount<15)){
             chara.style.top = (charaTop-6)+"px";
+            audio.play();
         }
         if(jumpCount>20){
             clearInterval(jumpInt);
