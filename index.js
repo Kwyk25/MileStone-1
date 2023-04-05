0e93dca6ebfced2aa1444c195aabcd1275f36eff
Kyle <kylev006@gmail.com>
2023-04-04 19:22:57 -0700

    fixed bugs and added start screen

--- index.js
+++ index.js
@@ -19,22 +19,23 @@ setInterval(function(){
     var pipesLeft = parseInt(window.getComputedStyle(pipes).getPropertyValue("left"));
     var gapTop = parseInt(window.getComputedStyle(gap).getPropertyValue("top"));
     var cTop = -(500-charaTop);
-    if(/*ground*/(charaTop>688)||(/*right to left*/(pipesLeft<186)&&/*top pipe*/(pipesLeft>-10)&&(/*top pipe*/(cTop<gapTop+247)||/*bottom pipe*/(cTop>gapTop+388)))){
-        alert("Game over. Score: "+(counter -1)); 
+    if(/*ground*/(charaTop>698)||(/*right to left*/(pipesLeft<173)&&/*top pipe*/(pipesLeft>-10)&&(/*top pipe*/(cTop<gapTop+247)||/*bottom pipe*/(cTop>gapTop+388)))){
+        window.location.href = "die.html"
+        alert("Score: "+(counter )); 
         chara.style.top = 200 + "px";
         counter=0;
     }parse
 },10);
 
 function jump(){
-    var audio = new Audio("./audio/sfx_wing.mp3");
+    // var audio = new Audio("./audio/sfx_wing.mp3");
     jumping = 1;
     let jumpCount = 0;
     var jumpInt = setInterval(function(){
         var charaTop = parseInt(window.getComputedStyle(chara).getPropertyValue("top"));
         if((charaTop>6)&&(jumpCount<15)){
             chara.style.top = (charaTop-6)+"px";
-            audio.play();
+            // audio.play();
         }
         if(jumpCount>20){
             clearInterval(jumpInt);
